64e47cc7973ba95296a63ed9ca411886a2ce01a0
mecoepcoo <340349237@qq.com>
2020-03-05 17:45:27 +0800

    chore(style): add postcss

--- config-overrides/index.js
+++ config-overrides/index.js
@@ -1,10 +1,40 @@
-const { override, addLessLoader } = require('customize-cra')
+const { override, addLessLoader, adjustStyleLoaders, addPostcssPlugins } = require('customize-cra')
 
 module.exports = {
   webpack: override(
     addLessLoader({
       sourceMap: true,
       modules: true,
+    }),
+    addPostcssPlugins(
+      [
+        require('postcss-aspect-ratio-mini')(),
+        require('postcss-write-svg')({ utf8: false }),
+        require('postcss-px-to-viewport')({
+          unitToConvert: 'px',
+          viewportWidth: 1080,
+          unitPrecision: 5,
+          viewportUnit: 'vmin',
+          fontViewportUnit: 'vmin',
+          selectorBlackList: ['.ignore', '.hairlines'],
+          minPixelValue: 1,
+          mediaQuery: false,
+          replace: true,
+          exclude: [],
+          landscape: false,
+          landscapeUnit: 'vmin',
+          landscapeWidth: 375
+        }),
+        require('cssnano')({
+          preset: 'advanced',
+          autoprefixer: true,
+          'postcss-zindex': false,
+        })
+      ]
+    ),
+    adjustStyleLoaders(({ use: [ , css, postcss, resolve, processor] }) => {
+      css.options.sourceMap = true
+      postcss.options.sourceMap = true
     })
   )
 }
